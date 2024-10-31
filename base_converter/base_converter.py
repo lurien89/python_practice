f853ecde3261cf31cb281d14f003db413819809e
Lurien <luri3n.the.watcher@proton.me>
2024-10-31 01:44:14 -0300

    Create rudimentary skeleton for decimal to binary function

--- base_converter/base_converter.py
+++ base_converter/base_converter.py
@@ -4,10 +4,26 @@ def binary():
     if output_type == 'd':
         input_binary = input("Input the binary number: ")
         # Need to verify that the input number is in fact binary
-        
+
+        # Binary to decimal converter here
+
 
 def decimal():
-    print("PLACEHOLDER")
+    output_type = input("Select the type of number you want to convert to (b/d/h): ")
+
+    if output_type == 'b':
+        input_decimal = input("Input de decimal number: ")
+        # Need to verify decimal
+        while input_decimal != 0:
+            input_decimal // 2
+            remainder = input_decimal % 2
+            rev_binary = rev_binary + str(remainder)
+
+        while len(rev_binary):
+            i = -1
+            binary_output = binary_output + binary_output[i]
+            i = i - 1
+        print(binary_output)
 
 def hexadecimal():
     print("PLACEHOLDER")
