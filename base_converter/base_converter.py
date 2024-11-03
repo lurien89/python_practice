d2fb5272cbcc03f4c090b34e29bb656479d7fdd7
Lurien <luri3n.the.watcher@proton.me>
2024-11-03 14:09:24 -0300

    Optimize code in decimal to binary
    Use lists instead of concatenating strings to make the code faster and
    more readable

--- base_converter/base_converter.py
+++ base_converter/base_converter.py
@@ -23,21 +23,27 @@ def binary():
 
 def decimal():
     output_type = input("Select the type of number you want to convert to (b/d/h): ")
+    binary_digits = []
 
     if output_type == 'b':
         input_decimal = int(input("Input the decimal number: "))
-        # Need to verify decimal
-        reversed_binary = ""
+        # reversed_binary = ""
         while input_decimal > 0:
             remainder = input_decimal % 2
-            input_decimal = input_decimal // 2
-            reversed_binary = str(reversed_binary) + str(remainder)
+            #input_decimal = input_decimal // 2
+            binary_digits.append(str(remainder))
+            #reversed_binary = str(reversed_binary) + str(remainder)
+            input_decimal //= 2
 
-        i = -1
-        binary_output = ""
-        while (i * -1) != (len(reversed_binary) +1):
-            binary_output = binary_output + reversed_binary[i]
-            i = i - 1
+        binary_digits.reverse()
+
+        #i = -1
+        #binary_output = ""
+        #while (i * -1) != (len(reversed_binary) +1):
+        #    binary_output = binary_output + reversed_binary[i]
+        #    i = i - 1
+
+        binary_output = ''.join(binary_digits)
 
         print(binary_output)
 
